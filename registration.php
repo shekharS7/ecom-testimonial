259e4a4d0da5cbfb2c0e5d8c7c0214416226de9e
Shekar Suman <shekharsuman97@gmail.com>
2024-07-29 16:32:02 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'RicherIndex_Testimonials',
+    'RicherIndex_Testimonial',
     __DIR__
 );
