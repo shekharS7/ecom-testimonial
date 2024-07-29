7760969edb60d3b5a4f75b78f1b0ac73c50e4c02
Shekar Suman <shekharsuman97@gmail.com>
2024-07-29 16:38:18 +0530

    Create InstallSchema.php

---
+++ Setup/InstallSchema.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace RicherIndex\Testimonial\Setup;
+
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * Install testimonials table
+     *
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
+    {
+        $installer = $setup;
+
+        $installer->startSetup();
+
+        /**
+         * Create table 'richerindex_testimonial'
+         */
+        $table = $installer->getConnection()->newTable(
+            $installer->getTable('richerindex_testimonial')
+        )->addColumn(
+            'testimonial_id',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            ['identity' => true, 'nullable' => false, 'primary' => true],
+            'ID'
+        )->addColumn(
+            'company_name',
+             \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            ['nullable' => false],
+            'Company Name'
+        )->addColumn(
+            'name',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            ['nullable' => false],
+            'Name'
+        )->addColumn(
+            'message',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            ['nullable' => false],
+            'Message'
+        )->addColumn(
+            'post',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            65000,
+            [],
+            'Post'
+        )->addColumn(
+            'image',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+            255,
+            ['nullable' => false],
+            'Image'
+        )->addColumn(
+            'status',
+            \Magento\Framework\DB\Ddl\Table::TYPE_SMALLINT,
+            null,
+            ['nullable' => false,'default' => '0'],
+            'Status'
+        )->addColumn(
+            'created_at',
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null,
+            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+            'Created At'
+        )->addColumn(
+            'updated_at', 
+            \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
+            null, 
+            ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE], 
+            'Updated At'
+        )->setComment(
+            'RicherIndex Testimonial Table'
+        );
+        $installer->getConnection()->createTable($table);
+
+        $installer->endSetup();
+    }
+}
