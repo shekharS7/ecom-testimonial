464a9c39cb4cd4414726476a3b1c47840b824714
Shekar Suman <shekharsuman97@gmail.com>
2024-08-02 10:45:47 +0530

    Create Delete.php

---
+++ Controller/Adminhtml/Testimonial/Delete.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Copyright © Shekhar Suman, 2024. All rights reserved.
+ * See COPYING.txt for license details.
+ * 
+ * @package     RicherIndex_Testimonial
+ * @version     1.0.0
+ * @license     MIT License (http://opensource.org/licenses/MIT)
+ * @autor       Shekhar Suman
+ */
+
+namespace RicherIndex\Testimonial\Controller\Adminhtml\Testimonial;
+
+use Magento\Backend\App\Action;
+use Magento\TestFramework\ErrorLog\Logger;
+
+class Delete extends \Magento\Backend\App\Action
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function _isAllowed()
+    {
+        return $this->_authorization->isAllowed('RicherIndex_Testimonial::testimonial_delete');
+    }
+
+    /**
+     * Delete action
+     *
+     * @return \Magento\Framework\Controller\ResultInterface
+     */
+    public function execute()
+    {
+        $id = $this->getRequest()->getParam('testimonial_id');
+        /** @var \Magento\Backend\Model\View\Result\Redirect $resultRedirect */
+        $resultRedirect = $this->resultRedirectFactory->create();
+        if ($id) {
+            try {
+                $model = $this->_objectManager->create('RicherIndex\Testimonial\Model\Testimonial');
+                $model->load($id);
+                $model->delete();
+                $this->messageManager->addSuccess(__('The testimonial has been deleted.'));
+                return $resultRedirect->setPath('*/*/');
+            } catch (\Exception $e) {
+                $this->messageManager->addError($e->getMessage());
+                return $resultRedirect->setPath('*/*/edit', ['testimonial_id' => $id]);
+            }
+        }
+        $this->messageManager->addError(__('We can\'t find a testimonial to delete.'));
+        return $resultRedirect->setPath('*/*/');
+    }
+}
