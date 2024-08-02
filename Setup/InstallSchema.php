d547b8e62dbe8500054852494eb4b26c1091a4d6
Shekar Suman <shekharsuman97@gmail.com>
2024-08-02 10:24:52 +0530

    Update InstallSchema.php

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -1,5 +1,13 @@
 <?php
-
+/**
+ * Copyright © Shekhar Suman, 2024. All rights reserved.
+ * See COPYING.txt for license details.
+ * 
+ * @package     RicherIndex_Testimonial
+ * @version     1.0.0
+ * @license     MIT License (http://opensource.org/licenses/MIT)
+ * @autor       Shekhar Suman
+ */
 namespace RicherIndex\Testimonial\Setup;
 
 use Magento\Framework\Setup\InstallSchemaInterface;
