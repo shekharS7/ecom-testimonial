b4fd6cc053535b17662b5136ed762548f45dd17c
Shekar Suman <shekharsuman97@gmail.com>
2024-08-02 10:51:36 +0530

    Create Status.php

---
+++ Model/Config/Source/Status.php
@@ -0,0 +1,49 @@
+<?php
+ 
+/**
+ * Copyright © Shekhar Suman, 2024. All rights reserved.
+ * See COPYING.txt for license details.
+ * 
+ * @package     RicherIndex_Testimonial
+ * @version     1.0.0
+ * @license     MIT License (http://opensource.org/licenses/MIT)
+ * @autor       Shekhar Suman
+ */
+
+namespace RicherIndex\Testimonial\Model\Config\Source;
+
+class Status implements \Magento\Framework\Data\OptionSourceInterface
+{
+    /**
+     * @var \RicherIndex\Testimonial\Model\Testimonial
+     */
+    protected $testimonial;
+
+    /**
+     * Constructor
+     *
+     * @param \RicherIndex\Testimonial\Model\Testimonial $testimonial
+     */
+    public function __construct(\RicherIndex\Testimonial\Model\Testimonial $testimonial)
+    {
+        $this->testimonial = $testimonial;
+    }
+
+    /**
+     * Get options
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
+        $options[] = ['label' => '', 'value' => ''];
+        $availableOptions = $this->testimonial->getAvailableStatuses();
+        foreach ($availableOptions as $key => $value) {
+            $options[] = [
+                'label' => $value,
+                'value' => $key,
+            ];
+        }
+        return $options;
+    }
+}
