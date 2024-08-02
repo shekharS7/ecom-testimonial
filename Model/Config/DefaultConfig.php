362c6f2d92851c59e0295e444942b5ef6d40ab7c
Shekar Suman <shekharsuman97@gmail.com>
2024-08-02 10:52:26 +0530

    Add files via upload

---
+++ Model/Config/DefaultConfig.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Copyright © Shekhar Suman, 2024. All rights reserved.
+ * See COPYING.txt for license details.
+ * 
+ * @package     RicherIndex_Testimonial
+ * @version     1.0.0
+ * @license     MIT License (http://opensource.org/licenses/MIT)
+ * @autor       Shekhar Suman
+ */
+namespace RicherIndex\Testimonial\Model\Config;
+
+class DefaultConfig
+{
+
+    /**
+     * testimonial profile image path
+     */
+    public const PROFILE_TMP_PATH = 'testimonial/tmp/profile/';
+}
